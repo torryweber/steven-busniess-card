5a3d95a05a20905a07e92bbe018163485a0d5556
torryweber <torryweber@gmail.com>
2026-08-13 20:32:50 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,45 @@
-a
+const CACHE_NAME = "steven-gst-v1";
+
+const ASSETS = [
+    "./",
+    "./index.html",
+    "./styles.css",
+    "./script.js",
+    "./manifest.json",
+    "./gst-logo.png",
+    "./favicon.ico",
+    "./icon-192.png",
+    "./icon-512.png"
+];
+
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(ASSETS))
+    );
+
+    self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys().then(keys =>
+            Promise.all(
+                keys
+                    .filter(key => key !== CACHE_NAME)
+                    .map(key => caches.delete(key))
+            )
+        )
+    );
+
+    self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(cachedResponse =>
+                cachedResponse || fetch(event.request)
+            )
+    );
+});
